ddcbc10b55062193a9c9aed09c372c2e6cc9e4cd
cludley <cludley@anamosa.k12.ia.us>
2025-12-04 19:14:57 +0000

    hi

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 class TaxCalculator{
+    #rate;
     
     constructor(){
-        this.rate = 15;
+        this.#rate = 15;
         this.state = 'TX';
         this.exempt = false;
     }
     
     calculate(){
         if(this.exempt){
-            return calculateExempt(1.37);
+            return this.#calculateExempt(1.37);
         }else{
-            return calculateNonExempt(5.72);
+            return this.#calculateNonExempt(5.72);
         }
+    }
+    
+    #calculateExempt(amount){
+    }
+    
+    #calculateNonExempt(amount){
     }
 }
